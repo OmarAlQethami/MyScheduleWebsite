76f97c982ac615e0185a8bee1c99fd5e00320d0f
Omar AlQethami <OmarAlQethami@gmail.com>
2024-11-02 16:58:39 +0300

    Designed initial student page

--- StudentPage.aspx
+++ StudentPage.aspx
@@ -1,4 +1,18 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StudentPage.aspx.cs" Inherits="MyScheduleWebsite.StudentPage" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <h2>This is the student page.</h2>
+    <div class="top-container center">
+        Steps will be here - TODO
+    </div>
+    <div class="main-container">
+         <div class="info">
+             <asp:Label ID="lblGreeting" runat="server" Text="Hello, name"></asp:Label>
+             <asp:Label ID="lblCurrentLevel" runat="server" Text="Current Level:"></asp:Label>
+             <asp:Label ID="lblHoursTaken" runat="server" Text="Hours Taken: TODO"></asp:Label>
+         </div>
+        <div class="subjects-container" id="subjectsContainer" runat="server">
+            <div class="subjects-row">
+                
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- StudentPage.aspx.cs
+++ StudentPage.aspx.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using MyScheduleWebsite.App_Code;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +15,125 @@ namespace MyScheduleWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                lblGreeting.Text = "Hello " + User.Identity.Name + "!";
+                Guid userId = GetUserId();
+                lblCurrentLevel.Text = "Current Level: " + getCurrentLevel(userId);
 
+                int universityId = getUniversityId(userId);
+                int majorId = getMajorId(userId);
+                BindSubjects(universityId, majorId);
+            }
         }
+        private Guid GetUserId()
+        {
+            if (Membership.GetUser() is MembershipUser user)
+            {
+                return (Guid)user.ProviderUserKey;
+            }
+            return Guid.Empty;
+        }
+
+        private int getCurrentLevel(Guid userId)
+        {
+            int currentLevel = 0;
+
+            CRUD myCrud = new CRUD();
+            string mySql = @"SELECT currentLevel FROM students WHERE UserId = @UserId";
+            Dictionary<string, object> myPara = new Dictionary<string, object>();
+            myPara.Add("@UserId", userId);
+
+            SqlDataReader dr = myCrud.getDrPassSql(mySql, myPara);
+            if (dr.Read())
+            {
+                currentLevel = dr.GetInt32(0);
+            }
+            dr.Close();
+            return currentLevel;
+        }
+
+        private int getUniversityId(Guid userId)
+        {
+            int universityId = 0;
+
+            CRUD myCrud = new CRUD();
+            string mySql = @"SELECT universityId FROM students WHERE UserId = @UserId";
+            Dictionary<string, object> myPara = new Dictionary<string, object>();
+            myPara.Add("@UserId", userId);
+
+            SqlDataReader dr = myCrud.getDrPassSql(mySql, myPara);
+            if (dr.Read())
+            {
+                universityId = dr.GetInt32(0);
+            }
+            dr.Close();
+            return universityId;
+        }
+
+        private int getMajorId(Guid userId)
+        {
+            int majorId = 0;
+
+            CRUD myCrud = new CRUD();
+            string mySql = @"SELECT majorId FROM students WHERE UserId = @UserId";
+            Dictionary<string, object> myPara = new Dictionary<string, object>();
+            myPara.Add("@UserId", userId);
+
+            SqlDataReader dr = myCrud.getDrPassSql(mySql, myPara);
+            if (dr.Read())
+            {
+                majorId = dr.GetInt32(0);
+            }
+            dr.Close();
+            return majorId;
+        }
+
+        protected DataTable GetSubjectsByUniversityAndMajor(int universityId, int majorId)
+        {
+            CRUD myCrud = new CRUD();
+            string mySql = @"SELECT * FROM subjects WHERE universityId = @universityId AND majorId = @majorId";
+
+            Dictionary<string, object> myPara = new Dictionary<string, object>();
+            myPara.Add("@universityId", universityId);
+            myPara.Add("@majorId", majorId);
+
+            DataTable dt = myCrud.getDTPassSqlDic(mySql, myPara);
+            return dt;
+        }
+
+
+        private void BindSubjects(int universityId, int majorId)
+        {
+            CRUD crud = new CRUD();
+            DataTable subjectsTable = GetSubjectsByUniversityAndMajor(universityId, majorId);
+
+            // Group subjects by level
+            var groupedSubjects = subjectsTable.AsEnumerable()
+                .GroupBy(row => row.Field<int>("subjectLevel")); // Ensure this column exists in your table
+
+            // Clear previous inner HTML to avoid duplication
+            subjectsContainer.InnerHtml = string.Empty;
+
+            // Iterate through each group of subjects
+            foreach (var group in groupedSubjects)
+            {
+                // Create a new row for this level
+                subjectsContainer.InnerHtml += "<div class='subjects-row'>";
+
+                // Add subjects for this level to the row
+                foreach (var subject in group)
+                {
+                    string subjectName = subject.Field<string>("subjectEnglishName");
+                    // Add any other fields if needed, e.g., subjectCode
+                    subjectsContainer.InnerHtml += $"<div class='subject'>{subjectName}</div>";
+                }
+
+                // Close the row div
+                subjectsContainer.InnerHtml += "</div>"; // Close subjects-row
+            }
+        }
+
+
     }
 }
